ba1753c1511aadc133df56b6a40c9645aa2ace76
Gazi Faizul Islam <22203175@iubat.edu>
2024-10-04 00:47:24 +0600

    first commit

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import "./Intro.css";
 import Vector1 from "../../img/Vector1.png";
 import Vector2 from "../../img/Vector2.png";
-import boy from "../../img/boy.png";
+import boy from "../../img/gazi.png";
 import glassesimoji from "../../img/glassesimoji.png";
 import thumbup from "../../img/thumbup.png";
 import crown from "../../img/crown.png";
@@ -28,10 +28,9 @@ const Intro = () => {
         <div className="i-name">
           {/* yahan change hy darkmode ka */}
           <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
-          <span>Andrew Thomas</span>
+          <span>Gazi Faizul Islam</span>
           <span>
-            Frontend Developer with high level of experience in web designing
-            and development, producting the Quality work
+          Front-End Developer | React.js Specialist | 2 Years Experience
           </span>
         </div>
         <Link to="contact" smooth={true} spy={true}>
@@ -39,8 +38,8 @@ const Intro = () => {
         </Link>
         {/* social icons */}
         <div className="i-icons">
-          <img src={Github} alt="" />
-          <img src={LinkedIn} alt="" />
+          <a href="https://github.com/faizul-gazi"><img src={Github} alt="" /></a>
+         <a href="https://www.linkedin.com/in/gazi-faizul-islam/"> <img src={LinkedIn} alt="" /></a>
           <img src={Instagram} alt="" />
         </div>
       </div>
@@ -64,7 +63,7 @@ const Intro = () => {
           transition={transition}
           className="floating-div"
         >
-          <FloatinDiv img={crown} text1="Web" text2="Developer" />
+          <FloatinDiv img={crown} text1="Front-End" text2="Developer" />
         </motion.div>
 
         {/* animation */}
